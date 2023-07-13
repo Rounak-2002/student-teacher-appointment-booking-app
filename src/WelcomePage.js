fc877b8b74357c489a58f950b79381a6e77c6372
rounak <rounakshaw903@gmail.com>
2023-07-13 12:32:29 +0530

    Initial commit

---
+++ src/WelcomePage.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./WelcomePage.css"; // import the CSS file for styling
+
+function WelcomePage() {
+  return (
+    <div className="welcome-page">
+      <h1>Student-Teacher Booking Appointment</h1>
+      <p>
+        Booking appointment systems, either online or through traditional
+        queueing systems, are now popular.
+      </p>
+      <p>
+        In this booking appointment systems are prevalent currently either
+        online or using traditional queuing systems.
+      </p>
+      <p>
+        Online booking system helps to simplify the booking process and raise
+        the efficiency in management.
+      </p>
+      <p>
+        Some student are not confidence and shy to ask anything in front of
+        people. An appointment with lecturers will help students to understand
+        more about their studies in case they are asking about studies.
+      </p>
+      <p>
+        This project proposes a web-based appointment booking system that
+        allows students and lecturers to be aware of their appointment time
+        regardless of where they are by using the web or mobile devices. By
+        connecting to the Internet, students and instructors can easily access
+        the system. It also permits students to send any message, including the
+        appointment's purpose and timing.
+      </p>
+    </div>
+  );
+}
+
+export default WelcomePage;
